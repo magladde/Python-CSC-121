42a5ef45f20fe54d793fda801658724b5cc1691a
Morgan <magladde@gmail.com>
2018-02-09 08:19:48 -0500

    Update

---
+++ Lesson 04/D5.py
@@ -0,0 +1,10 @@
+# Life signs program
+
+# program introduction
+print('This program will take user input for age and calculate the')
+print('total breaths and heart beats that person has had in their ')
+print('lifetime based off of perdetermined averages.')
+
+# variable initialization
+infant_min_breaths = 25
+infant_max_breaths = 60
