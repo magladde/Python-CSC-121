12f13e159ad3a7706cce8418536ad27270b40a1f
magladde@gmail.com <magladde@gmail.com>
2018-03-06 13:52:20 -0500

    Lesson 08 - problems in the assigned reading + lab problems

---
+++ Lesson 08/Lab08P3.py
@@ -0,0 +1,6 @@
+# Lesson 08 lab problem 3
+print('This program creates a list of odd numbers in the range of your choice.')
+start_num = int(input('Enter a starting number: '))
+end_num = int(input('Enter ending number: '))
+my_list = list(num for num in range(start_num, end_num + 1) if num % 2 == 1)
+print(my_list)
---
+++ Lesson 08/Lab08P4.py
@@ -0,0 +1,9 @@
+# Lesson 08 lab problem 4
+average = lambda x: sum(x) / len(x)
+
+list1 = [2, 1, 5, 9, 8]
+list1_avg = average(list1)
+print('list1 average:', list1_avg)
+list2 = [17, 5, 2, 4]
+list2_avg = average(list2)
+print('list2 average:', list2_avg)
