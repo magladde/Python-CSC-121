47601dd425732456c0d3e155382520add89fae8f
morgan <magladde@gmail.com>
2018-03-21 17:54:00 -0400

    Lesson 10

---
+++ Lesson 10/Lab10P4.py
@@ -0,0 +1,17 @@
+# Lesson 10 lab problem 4
+
+def main():
+    # get user input
+    user_input = input('Enter a string: ')
+    user_input = user_input.upper()
+    print(user_input)
+
+    number_characters = len(user_input)
+    print(number_characters)
+
+    print(alphabet)
+
+    for i in range(number_characters):
+        
+
+main()
